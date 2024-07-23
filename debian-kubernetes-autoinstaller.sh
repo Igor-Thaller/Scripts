df0ebd42dd9efa534a038d24fd9f3e48a3327353
Igor Thaller <igor.thaller@aon.at>
2024-07-23 09:58:25 +0200

    Made the sudo rights management better

--- debian-kubernetes-autoinstaller.sh
+++ debian-kubernetes-autoinstaller.sh
@@ -31,14 +31,14 @@ function removeDocker {
     # Removing docker if installed
     echo "Removing docker if installed"
     for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
-    apt-get purge aufs-tools docker-ce docker-ce-cli containerd.io pigz cgroupfs-mount -y
-    apt-get purge kubeadm kubernetes-cni -y
-    rm -rf /etc/kubernetes
-    rm -rf $HOME/.kube/config
-    rm -rf /var/lib/etcd
-    rm -rf /var/lib/docker
-    rm -rf /opt/containerd
-    apt autoremove -y
+    sudo apt-get purge aufs-tools docker-ce docker-ce-cli containerd.io pigz cgroupfs-mount -y
+    sudo apt-get purge kubeadm kubernetes-cni -y
+    sudo rm -rf /etc/kubernetes
+    sudo rm -rf $HOME/.kube/config
+    sudo rm -rf /var/lib/etcd
+    sudo rm -rf /var/lib/docker
+    sudo rm -rf /opt/containerd
+    sudo apt autoremove -y
     dashLine
 }
 
@@ -81,8 +81,8 @@ function updatePackages {
     line
     # Updating packages
     echo "Updating all apt packages"
-    apt update
-    apt dist-upgrade
+    sudo apt update
+    sudo apt dist-upgrade
     line
 }
 
@@ -91,16 +91,16 @@ function fetchAssociatedVersionData {
     line
     echo "Fetching data"
     if [version != "stable"]; then
-        curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+        sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
     else
-        curl -LO https://dl.k8s.io/release/${version}/bin/linux/amd64/kubectl
+        sudo curl -LO https://dl.k8s.io/release/${version}/bin/linux/amd64/kubectl
     fi
 
     dashLine
 
     # Validate
     echo "Validating the binary"
-     curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+     sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 
     echo "Validation results"
     echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
@@ -113,7 +113,7 @@ function installMinikube {
     line
 
     echo "Installing minikube"
-    curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    sudo curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
     sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
 
     line
